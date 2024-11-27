82d35d88c37dba1cb7346140f9be7d075a584ef3
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-11-27 15:40:52 +0900

    feat: Prismaクライアントを使ってToDoリストを取得するAPIを実装

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const todos = await prisma.toDo.findMany();
+  res.json(todos);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on PORT 3000");
+});
