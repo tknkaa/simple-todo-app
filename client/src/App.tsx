3de812060a85e0a65d24fc7d1a90199f0904314a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-11-27 16:06:35 +0900

    feat: ToDoを表示

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+type ToDo = {
+  id: number;
+  title: string;
+};
 
+function App() {
+  const [toDos, setToDos] = useState<ToDo[]>([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await fetch("http://localhost:3000/", {
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+      });
+      const fetchedToDos = await res.json();
+      setToDos(fetchedToDos);
+    };
+    fetchTodos();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {toDos.map((todo) => {
+          return <li key={todo.id}>{todo.title}</li>;
+        })}
+      </ul>
     </>
   );
 }
